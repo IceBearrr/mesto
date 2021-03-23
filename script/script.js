d4800ea22388c7c50d7f00d97babb2132729f43c
IceBearrr <chirkarofl@gmail.com>
2021-03-24 00:08:20 +0300

    all ok

--- script/script.js
+++ script/script.js
@@ -147,7 +147,7 @@ const propertiesValidation = {
     formSelector: '.popup__form',
     inputSelector: '.popup__input',
     submitButtonSelector: '.popup__button',
-    inactiveButtonClass: 'popup__button_disabled',
+    inactiveButtonClass: 'popup__button-deactive',
     inputErrorClass: 'popup__input_type_error',
     errorClass: 'popup__error_visible'
 }
