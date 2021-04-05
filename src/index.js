d052b5db93cc453b9ebbf55e9a1702fa0739ab66
Михаил Греков <mgrekov@MacBook-Pro-Mihail.local>
2021-04-05 13:52:40 +0300

    webpack

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-import Card from './components/Card.js';
-import UserInfo from './components/UserInfo.js';
-import Section from './components/Section.js';
-import FormValidator from './components/FormValidator.js';
-import {initialCards, formElementEdit, formElementAdd, elementCell, addButton, editButton} from './utils/constants.js';
-import PopupWithForm from './components/PopupWithForm.js';
-import PopupWithImage from './components/PopupWithImage.js';
+import '../pages/index.css'; // добавьте импорт главного файла стилей
+
+
+import Card from '../components/Card.js';
+import UserInfo from '../components/UserInfo.js';
+import Section from '../components/Section.js';
+import FormValidator from '../components/FormValidator.js';
+import {initialCards, formElementEdit, formElementAdd, elementCell, addButton, editButton} from '../utils/constants.js';
+import PopupWithForm from '../components/PopupWithForm.js';
+import PopupWithImage from '../components/PopupWithImage.js';
 
 const popup_tag = '.popup_img';
 
